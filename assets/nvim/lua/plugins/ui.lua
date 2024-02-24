e1048a8bc359237b81da485d18ce5c1bbb60f2ce
Ethan Bickel <notethanbickel@gmail.com>
2024-02-24 15:23:46 -0600

    initial commit

---
+++ assets/nvim/lua/plugins/ui.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "akinsho/bufferline.nvim",
+    enabled = false,
+  },
+  {
+    "folke/which-key.nvim",
+    enabled = true,
+  },
+  {
+    "folke/noice.nvim",
+    opts = {
+      cmdline = {
+        enabled = true,
+        view = "cmdline",
+      },
+    },
+  },
+}
---
+++ assets/nvim/lua/plugins/undo.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "mbbill/undotree",
+    keys = {
+      {
+        "<leader>t",
+        "<cmd>UndotreeToggle<CR>",
+      },
+    },
+  },
+}
