8bbaa3ad331eba8161ec8316d48ae1ba9e5fefb1
Ethan Bickel <notethanbickel@gmail.com>
2024-03-17 15:46:22 -0500

    configure default wayland stuff

---
+++ home/programs/wayland/default.nix
@@ -0,0 +1,31 @@
+{
+  pkgs,
+  self,
+  ...
+}: {
+  imports = [
+    ./hyprland
+    ./hyprlock.nix
+    ./wlogout.nix
+  ];
+  home.packages = with pkgs; [
+    #screenshot
+    grim
+    slurp
+
+    #utils
+    self.packages.${pkgs.system}.wl-ocr
+    wl-clipboard
+    wl-clip-persit
+    cliphist
+    wl-screenrec
+    wlr-randr
+  ];
+
+  # useful vars
+  home.sessionVariables = {
+    QT_QPA_PLATFORM = "wayland";
+    SDL_VIDEODRIVER = "wayland";
+    XDG_SESSION_TYPE = "wayland";
+  };
+}
