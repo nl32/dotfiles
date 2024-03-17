9b4711102e9f99d1aab735f393424b390b4fcdf7
Ethan Bickel <notethanbickel@gmail.com>
2024-03-17 16:38:23 -0500

    reorg system default

--- system/default.nix
+++ system/default.nix
@@ -2,9 +2,12 @@
   imports = [
     ./core/boot.nix
     ./core/default.nix
+    ./network
+
+    ./programs
+
     ./services
     ./services/greetd.nix
     ./services/pipewire.nix
-    ./network
   ];
 }
