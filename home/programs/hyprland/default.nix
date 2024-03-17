aa903c29cd743c9c9a1ca48e9eab6e86debc9bec
Ethan Bickel <notethanbickel@gmail.com>
2024-03-16 20:58:56 -0500

    hyprland things

--- home/programs/hyprland/default.nix
+++ home/programs/hyprland/default.nix
@@ -3,7 +3,18 @@
   pkgs,
   ...
 }: {
-  wayland.windowManager.hyprland.enable = true;
+  wayland.windowManager.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+    systemd.enable = true;
+    settings = {
+      "$mod" = "SUPER";
+      bind = [
+        "$mod, Q, exec, kitty"
+        "$mod, C, killactive"
+      ];
+    };
+  };
   home.packages = with pkgs; [
     wl-clip-persist
     wl-clipboard
