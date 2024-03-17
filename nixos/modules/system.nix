201499684e8e3b64577f4daad668a0665a9c8ca7
Ethan Bickel <notethanbickel@gmail.com>
2024-03-17 00:33:59 -0500

    fix typo

--- nixos/modules/system.nix
+++ nixos/modules/system.nix
@@ -1,9 +1,12 @@
-{pkgs,lib,...}:{
-    services = {
-      dbus = {
-          packages = with pkgs; [dconf gcr udisks2];
-          enable = true;
-        };
-
-      }
-  }
+{
+  pkgs,
+  lib,
+  ...
+}: {
+  services = {
+    dbus = {
+      packages = with pkgs; [dconf gcr udisks2];
+      enable = true;
+    };
+  };
+}
